0af05e1e48638634c692ae5dd9ebcb03c694e0a0
[Michael] <michael@lewiscommunications.com>
2015-09-29 18:24:15 -0500

    finish lab

--- client/js/main.js
+++ client/js/main.js
@@ -1,41 +1,89 @@
-angular.module('myApp, []');
-    .controller(tweetsController, ['$scope', $http, function($scope, $http){
-        $scope.tweets = [];
-        getTweets();
-        $scope.user = 'Michael';
-        $scope.text = '';
-        $scope.tweet(){
-            username: $scope.user
-            text: $scope.text
-        };
-        $scope.sendTweet = function(){
-            $http.post('/messages', JSON.stringify($scope.text))
-                .then(function(){
-                    $scope.tweets.shift($scope.text)
-                    $scope.text = ';'
-                }).catch function(err){
-                    alert('Something is wrong');
-                
-                }
-        }
-    }])
-function getTweets(){
+var myApp = angular.module('myApp', ['ngRoute']);
+myApp.controller('TweetsController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+    $scope.tweets = [];
+    $scope.tweetUser = $routeParams.user + ': ';
     $http.get('/messages')
-    .then (function(result){
-        var arr = result.split('\n');
-        result.date.forEach(function(tweets){
-            $scope.tweets.unshift(JSON.parse(tweet))
-        }
-        )
-    })
-}
+        .success(function (messages) {
+            //$scope.tweets = messages.reverse();
+            messages.forEach(function (message) {
+                if (message.userName === $scope.tweetUser) {
+                    $scope.tweets.unshift(message);
+                }
+            })
+
+        })
+        .error(function (err) {
+            console.error(err);
+        })
+    $scope.postTweet = function () {
+        var tweet = {
+            text: $scope.tweet,
+            userName: $scope.tweetUser
+        };
+        $http.post('/messages', tweet)
+            .success(function () {
+                $scope.tweets.unshift(tweet);
+            })
+            .error(function (err) {
+                console.error(err);
+            })
+    }
+}]);
+
+myApp.controller('welcomeController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+    $scope.username = '';
+    $scope.login = function () {
+        $location.path('/tweets/' + $scope.username);
+    }
+
+}]);
+
+myApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/login', {
+            templateUrl: '../views/welcome.html',
+            controller: 'welcomeController'
+        })
+        .when('/tweets/:user', {
+            templateUrl: '../views/tweets.html',
+            controller: 'TweetsController'
+        })
+        .otherwise({
+            redirectTo: '/login'
+        });
+});
 
 
 
 
-
-
-
+//     myApp.controller('tweetsController', ['$scope', $http, function($scope, $http){
+//         
+//         getTweets();
+//         $scope.user = 'Michael';
+//         $scope.text = '';
+//         $scope.tweet(){
+//             username: $scope.user
+//             text: $scope.text
+//         };
+//         $scope.sendTweet = function(){
+//             $http.post('/messages', JSON.stringify($scope.text))
+//                 .then(function(){
+//                     $scope.tweets.shift($scope.text)
+//                     $scope.text = ';'
+//                 }).catch function(err){
+//                     alert('Something is wrong');
+                
+//                 }
+//         }
+//     }])
+// function getTweets(){
+//     $http.get('/messages')
+//     .then (function(result){
+//         var arr = result.split('\n');
+//         result.data.forEach(function(tweets){
+//             $scope.tweets.unshift(JSON.parse(tweet))
+//         });
+//     })};
 
 
 
---
+++ index.js
@@ -0,0 +1,33 @@
+/* Require node modules to implement */
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+var app = require('./server/app.js');
+
+/*createServer takens in a function to handle requests. Here is where you can create a handler for get and post requests. Note: req(request) and res(response) come from node's http module. They include both incoming information like urls and outgoing like content */
+
+/*In order to complete the project, this callback will need to handle get requests, post requests and server up other files like css.
+
+Hint: creating a function to replace the anonymous function may be useful.
+		EXAMPLE:
+		function requestHandler(req, res) {
+			if(request url === '/'){
+				// handle this way
+			} else if(request url === '/messages'){
+				if(request method  === 'GET'){
+					// handle this way
+				}
+
+				...
+		};
+
+		var server = http.createServer(requestHandler)
+
+
+*/
+var server = http.createServer(app.handleRequests);
+var port = 3000;
+	
+server.listen(port);
+console.log('Listening on port', port);
